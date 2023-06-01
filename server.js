7256439ac1df7f9c6b3491f89646c46d47000469
Jabo Jean Marie <jabojeanmarie5@gmail.com>
2023-06-01 14:21:12 +0200

    added cors

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const { errorHandler } = require('./middleware/errorMiddleWare')
 const connectDB = require('./config/db')
+const cors = require('cors')
 
 connectDB();
 
 const app = express()
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
